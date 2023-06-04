695ee14576260799c833a0ea025fddd66a5eabcb
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-06-04 17:47:27 +0530

    Time: 20 ms (32.66%), Space: 13.4 MB (30.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -6,14 +6,17 @@ public:
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
                 if(matrix[i][j]==0){
-                    r[i]++;
-                    c[j]++;
+                    r[i]=1;
+                    c[j]=1;
                 }
             }
         }
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                if(r[i]!=0 || c[j]!=0){
+                if(r[i]==0 && c[j]==0){
+                    matrix[i][j]=matrix[i][j];
+                }
+                else{
                     matrix[i][j]=0;
                 }
             }
