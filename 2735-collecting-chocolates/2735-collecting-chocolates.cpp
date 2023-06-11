706a1976a39de56084d97d48cae925935d36cbff
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-06-11 14:02:15 +0530

    Time: 433 ms (30.00%), Space: 61.1 MB (20.00%) - LeetHub

---
+++ 2735-collecting-chocolates/2735-collecting-chocolates.cpp
@@ -0,0 +1,27 @@
+typedef long long ll;
+class Solution {
+private:
+    void change(vector<int> &nums) {
+        int last = nums[nums.size() - 1];
+        for(int i = nums.size() - 2; i >=0 ; i--) {
+            nums[i+1] = nums[i];
+        }
+        nums[0] = last;
+    }
+public:
+    long long minCost(vector<int>& nums, int x) {
+        int n = nums.size();
+        vector<ll> cost(n, LONG_MAX);
+        ll mini = LONG_MAX;
+        for(int i = 0; i < n; i++) {
+            change(nums);
+            ll totalCost = (ll)(x) * (ll)(i);
+            for(int j = 0; j < n; j++) {
+                cost[j] = min(cost[j],(ll)nums[j]);
+                totalCost+=cost[j];
+            }
+            mini = min(mini, totalCost);
+        }
+        return mini;  
+    }
+};
