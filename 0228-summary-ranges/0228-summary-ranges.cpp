0a1ee39c257f09ea6a6fe7f12ff0f072fea7ff28
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-06-12 12:47:50 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (57.22%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& arr) {
+        int n = arr.size(); 
+        vector<string> ans; 
+        string temp = "";
+        for(int i = 0; i < n; i++)
+        {
+            int j = i;
+            
+            while(j + 1 < n && arr[j + 1] == arr[j] + 1)
+            {
+                j++;
+            }
+            if(j > i)
+            {
+                temp += to_string(arr[i]);
+                temp += "->"; 
+                temp += to_string(arr[j]); 
+            }
+            else 
+            {
+                temp += to_string(arr[i]); 
+            }
+            
+            ans.push_back(temp); 
+            temp = "";
+            i = j; 
+        }
+        return ans;
+    }
+};
