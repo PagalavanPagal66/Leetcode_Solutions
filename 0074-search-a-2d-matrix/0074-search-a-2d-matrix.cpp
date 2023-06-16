5259453609844e56c2bb1d891436407263eac24f
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-06-16 17:00:13 +0530

    Time: 10 ms (17.59%), Space: 9.5 MB (26.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row=matrix.size(),col=matrix[0].size();
-        int i=0,j=col-1;
-        while(i<row && j>=0){
-            if(matrix[i][j]==target){
-                return true;
-            }
-            if(matrix[i][j]>target){
-                j--;
-            }
-            else{
-                i++;
-            }
+    bool binarySearch(vector<int>&arr, int left, int right,int target){
+        if(left==right && arr[left]==target)
+            return true;
+        if(left<right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]==target)
+                return 1;
+            if(arr[mid]<target)
+                return binarySearch(arr,mid+1,right,target);
+            if(arr[mid]>target)
+                return binarySearch(arr,left,mid,target);
         }
-        return false;
+        return 0;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(); 
+        int m=matrix[0].size(); 
+        int wanted=-1;
+        for(int i=0; i<n; i++){
+            if(matrix[i][0]<target && target<matrix[i][m-1])
+                wanted=i;
+            if(matrix[i][0]==target || matrix[i][m-1]==target) return 1;
+        }
+        if(wanted==-1) return 0;
+        return binarySearch(matrix[wanted],0,m-1,target);
     }
 };
