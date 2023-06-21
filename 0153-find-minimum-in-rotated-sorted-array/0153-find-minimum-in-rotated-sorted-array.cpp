b6bcb3fd7fb1f66200ea00ed94a0515649044e34
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-06-21 20:56:08 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (8.63%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -8,13 +8,16 @@ public:
     int ans=INT_MAX;
     while(start<=end)
     {
-        int mid=(start)+(end-start)/2;
-        if(nums[start] <= nums[mid]){
-                ans = min(nums[start], ans);
-                start = mid + 1;
+        int mid=(start)+(end-start)/2; //(strt+end)/2
+        if(nums[start] <= nums[mid]){  // first half is sorted .. first half doesnt mean whole array bu the array we consider for binary search (strt to mid)
+                ans = min(nums[start], ans); // since (strt to mid) is sorted we have to take the smallest element from that half 
+                start = mid + 1; // we have taken the smallest element in range of(strt to mid). So going to search for minimum element in the other half
         }else{
+             // second half is sorted....(mid to end)
                 ans = min(nums[mid], ans);
+            // smallest element in the second half is mid. Since it lies left in this range of elements(mid to end)
                 end = mid-1;
+            // we have taken the smallest element in the second half. So going to find minimum in the other half (STRT to MID)
         }
     }
     return  ans;
