aa94edfe5839a16239302a00908ebb29e9b46845
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-06-21 21:09:19 +0530

    Time: 3 ms (79.24%), Space: 11.1 MB (16.55%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,24 +5,24 @@ public:
         int n=nums.size();
         int end=n-1;
         while(strt<=end){
-            int mid=strt+(end-strt)/2;
+            int mid=strt+(end-strt)/2; //(strt+end)/2
             if(nums[mid]==target){
-                return mid;
+                return mid; //answer found
             }
-            if(nums[strt]<=nums[mid]){
-                if(nums[mid]>=target && nums[strt]<=target){
-                    end=mid-1;
+            if(nums[strt]<=nums[mid]){ // first half (Strt to mid) is sorted (It can able to form a range)
+                if(nums[mid]>=target && nums[strt]<=target){ // checking whether the target will fall on that range (Strt to mid)
+                    end=mid-1; // I will surely have to find within that range. So eliminating second half and continue search in the first half (strt to mid)
                 }
-                else{
-                    strt=mid+1;
+                else{ //target will not fall on that range 
+                    strt=mid+1; // target will never be found within that range (strt to mid)
                 }
             }
-            else{
-                 if(nums[mid]<=target && nums[end]>=target){
-                    strt=mid+1;
+            else{ //second half is sorted (mid to end) will form a range
+                if(nums[mid]<=target && nums[end]>=target){  // we have find whether the target will fall on that range (mid to end)
+                    strt=mid+1; // Target is on that range. So searching only the second half
                 }
-                else{
-                    end=mid-1;
+                else{ //Second half (mid to end) forms a range. But it is no the range that the target exists. So searching in second half never produce answer
+                    end=mid-1; // moving to first half
                 }
             }
         }
