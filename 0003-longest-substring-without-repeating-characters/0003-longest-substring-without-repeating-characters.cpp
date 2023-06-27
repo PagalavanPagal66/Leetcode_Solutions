fe8f55849773c12c9926bf1c6e9f1fe8a150029d
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-06-27 21:12:32 +0530

    Time: 23 ms (56.24%), Space: 8.9 MB (45.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.length()==0)return 0;  
-        unordered_map<char,int> m;   
-        int i=0,j=0,ans=INT_MIN; 
-        while(j<s.length())   
+        if(s.length()==0)return 0;   // there is no string, how to form a substring
+        unordered_map<char,int> m;   //matching character with integers
+        int i=0;  //left border
+        int j=0; // right border
+        int ans=INT_MIN;  // taking the minimal value among the integer
+        while(j<s.length())   // for (j = 0 ; j < n ; j++)
         {
-            m[s[j]]++;  
-            if(m.size()==j-i+1)  
+            m[s[j]]++;        // marking the j th indexed character as seen
+            if(m.size()==j-i+1)  //comparing the map size -> unique elements is equal to the number of characters in that string . So  no where repeatation occurs.
             {
-                ans = max(ans,j-i+1); 
+                ans = max(ans,j-i+1);
+                // ans = max(ans,mp.size());
             }
-            else if(m.size()<j-i+1)   
+            else if(m.size()<j-i+1)   // map size is lesser than the characters count in that substring. Somewhere repeatation occurs. Whenever repeatation occurs the map will go to the same character to map twice. so this change occurs. 
             {
-                while(m.size()<j-i+1)  
+                while(m.size()<j-i+1)  // untill the repeatation is happening we are reducing the left border 
                 {
-                    m[s[i]]--;   
-                    if(m[s[i]]==0)  
+                    m[s[i]]--; // we have to reduce the count of the characters outside the leftborder    
+                    if(m[s[i]]==0)  // we have no characters
                     {
-                        m.erase(s[i]);
+                        m.erase(s[i]); // we are totally removing that character key pair from that map
                     }
-                    i++;   
+                    i++;   //moving the left border towards the right 
                 }
             }
-             j++; 
+            j++; 
         }
         return ans;
     }
