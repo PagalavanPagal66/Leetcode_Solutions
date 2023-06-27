8183fb94d0b68d6eb97cc91dcfb2f0b7215364e9
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-06-27 21:14:55 +0530

    Time: 168 ms (8.68%), Space: 93.2 MB (99.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if (prices.size() < 2) return 0;
-        int i = 0, j = 1;
+        if (prices.size() ==1 ) return 0;
+        int i = 0, n = prices.size();
         int ans = 0;
-        while(j < prices.size()) {
-            if(prices[i] < prices[j]) {
-                int profit = prices[j] - prices[i];
-                ans = max(profit, ans);
+        // i ->buying products index , j->selling products index
+        for(int j=1 ; j < n ;j++) { // moving j from 1 to (n-1)
+            if(prices[i] < prices[j]) { // buying price is less than the selling price.... profit gaining
+                int profit = prices[j] - prices[i]; 
+                ans = max(profit, ans); // tracking the maximum profit
             }
-            else i = j;
-            j++;
+            else{ 
+        //selling price is less than or equal to buying price . so here loss is happening . But why cant we change this buying price as the next price. Since , j th index value has lesser product price then i th index.
+                i = j;
+            }
         }
         return ans;
     }
