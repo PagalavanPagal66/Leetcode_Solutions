b8c6e43fe2ad30dd6e22ef1468150ab851cae050
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-07-05 19:56:15 +0530

    Added solution - LeetHub

---
+++ Stock buy and sell II - GFG/stock-buy-and-sell-ii.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+int stockBuyAndSell(int n, vector<int> &prices) {
+        // code here
+        if(n < 2){
+            return 0;
+        }
+        int max_profit = 0;
+        
+        for(int i=1; i<n; i++) {
+            if(prices[i] > prices[i-1]){
+                max_profit +=  prices[i] - prices[i-1];
+            }
+        }
+        return max_profit;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> prices(n);
+        Array::input(prices,n);
+        
+        Solution obj;
+        int res = obj.stockBuyAndSell(n, prices);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
