7eaeb946dbb0603b2c513bac6780e3743fd90ecb
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-08-07 09:15:25 +0530

    Time: 59 ms (99.57%), Space: 45.6 MB (73.07%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
+    int part(vector<int>& nums, int l, int r) {
+        int tar = nums[r];
+        int i = l - 1;
+        for (int j = l; j < r; j++)
+            if (nums[j] <= tar)
+                swap(nums[j], nums[++i]);
+        swap(nums[i + 1], nums[r]);
+        return i + 1;
+    }
+    
+    int random_part(vector<int>& nums, int l, int r) {
+        int res = rand() % (r - l + 1) + l;
+        swap(nums[res], nums[r]);
+        return part(nums, l, r);
+    }
+    
+    int quick_part(vector<int>& nums, int l, int r, int target) {
+        int next = random_part(nums, l, r);
+        if (next == target) {
+            return nums[next];
+        } else {
+            return (next > target) ? quick_part(nums, l, next - 1, target) : quick_part(nums, next + 1, r, target);
+        }
+    }
+    
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(int i=0;i<k;i++){
-            pq.push(nums[i]);
-        }
-        for(int i=k;i<nums.size();i++){
-            if(nums[i]>pq.top()){
-                pq.pop();
-                pq.push(nums[i]);
-            }
-        }
-        return pq.top();
+        return quick_part(nums, 0, nums.size() - 1, nums.size() - k);
     }
 };
